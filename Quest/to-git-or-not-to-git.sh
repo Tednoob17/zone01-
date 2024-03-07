aba8cf358ae99a1d1bfeb7877496765df3969bde
mouhamed amine tareza <123250253+tarzacol@users.noreply.github.com>
2024-03-07 16:13:11 +0100

    Update to-git-or-not-to-git.sh

--- Quest/to-git-or-not-to-git.sh
+++ Quest/to-git-or-not-to-git.sh
@@ -1,2 +1,2 @@
-returnValue=`curl -s https://learn.zone01dakar.sn/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"mlastmock103\"}}){id}}"}'`
+returnValue=`curl -s https://learn.zone01.sn/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"mlastmock103\"}}){id}}"}'`
 echo $returnValue | cut -d ':' -f 4 | cut -d '}' -f 1
