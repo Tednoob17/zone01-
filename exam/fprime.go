2e115e4795fe6b4a8d9da38c0540968a54af7532
mtarza <amintarzae@gmail.com>
2024-03-09 00:00:18 +0100

    exam

--- exam/fprime.go
+++ exam/fprime.go
@@ -6,30 +6,30 @@ import (
 	"strconv"
 )
 
+func fprime(value int) {
+	if value == 1 {
+		return
+	}
+	divisionIterator := 2
+	for value > 1 {
+		if value%divisionIterator == 0 {
+			fmt.Print(divisionIterator)
+			value = value / divisionIterator
+
+			if value > 1 {
+				fmt.Print("*")
+			}
+			divisionIterator--
+		}
+		divisionIterator++
+	}
+	fmt.Println()
+}
+
 func main() {
 	if len(os.Args) == 2 {
 		if i, err := strconv.Atoi(os.Args[1]); err == nil {
 			fprime(i)
 		}
 	}
-}
-
-func fprime(i int) {
-	if i == 1 {
-		return
-	}
-	div := 2
-	for i > 1 {
-		if i%div == 0 {
-			fmt.Print(div)
-			i = i / div
-
-			if i > div {
-				fmt.Print("*")
-			}
-			div--
-		}
-		div++
-	}
-	fmt.Println()
 }
